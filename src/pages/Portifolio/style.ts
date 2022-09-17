7ab45b11ddf9d8c6b48b186080a3af529238f940
Myke Vida de Macedo <myke.programacao@gmail.com>
2022-09-16 21:59:16 -0300

    feat:finalização estilo na pagina protifolio

--- src/pages/Portifolio/style.ts
+++ src/pages/Portifolio/style.ts
@@ -14,4 +14,29 @@ export const Styled = styled.div`
     background-color: #1b2838;
 
     overflow: hidden;
+
+    .block__title{
+        font-size: 30px;
+
+        font-weight: 700;
+
+        color: #F5F5F5;
+
+        @media(min-width:400px){
+            font-size: 35px;
+        }
+    }
+
+    .block__navegation{
+        box-sizing: border-box;
+
+        width: 100%;
+
+        display: flex;
+        justify-content: space-between;
+
+        padding-left: 5px;
+
+        margin: 10px 0px;
+    }
 `
